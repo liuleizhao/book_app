31059a96bf4eff6ce8d6d1dfc596f5cd9cc8a999
liuleizhao <171306528@qq.com>
2018-02-05 18:49:14 +0800

    1

--- book_app/js/commom.js
+++ book_app/js/commom.js
@@ -1,9 +1,11 @@
+/*配置模块信息*/
 define(function(){
    require.config({
 		baseUrl: "js/",
 		paths: {
-			"jquery": "lib/jquery-1.11.0.js",
-			"app": "app/app.js"
+			"jquery": "lib/jquery/jquery-3.3.1.min.js",
+			"app": "app/app.js",
+			"mui":"mui/mui.min.js"
 		},shim:{ 
 			"underscore":{
 				"exports":"_"
